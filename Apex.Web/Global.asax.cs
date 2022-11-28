910f29d16e1ba0a8b420a79dee4f6dd3c26f9a7a
peter.marinov <peter.marinov@americaneagle.com>
2022-11-28 23:56:09 +0200

    Adding Dev Feather invoker to show errors to developers when in Debug mode True

---
+++ Apex.Web/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ApexWeb.App_Start;
+using Telerik.Sitefinity.Abstractions;
+
+namespace ApexWeb
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Bootstrapper.Bootstrapped -= OnBootstrapped;
+            Bootstrapper.Bootstrapped += OnBootstrapped;
+        }
+
+        private void OnBootstrapped(object sender, EventArgs e)
+        {
+            SitefinityConfig.RegisterDevModeActionInvoker(ObjectFactory.Container);
+        }
+    }
+}
