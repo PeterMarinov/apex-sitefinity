d5bf88f6fda0d9a8d40fd9ecfe1d1ac93f21d410
peter.marinov <peter.marinov@americaneagle.com>
2022-11-29 23:50:33 +0200

    Adding Contact Info widget for the footer

---
+++ Apex.Web/Mvc/Controllers/ContactInfoController.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using ApexWeb.Mvc.Models;
+using Telerik.Sitefinity.Mvc;
+
+namespace ApexWeb.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "ContactInfo", Title = "Contact Info", SectionName = "Apex Widgets")]
+    public class ContactInfoController : Controller
+    {
+        private string _viewName = "Index";
+
+        #region Widget Properties
+
+        [Required]
+        [Description("Add title for the widget")]
+        public string Title { get; set; } = "Address";
+
+        [Required]
+        [Description("Add address for contact info")]
+        public string Address { get; set; } = "123 Street, New York, USA";
+
+        [Required]
+        [Description("Add phone number for contact info")]
+        public string PhoneNumber { get; set; } = "+012 345 67890";
+
+
+        [Required]
+        [Description("Add email address for contact info")]
+        [EmailAddress(ErrorMessage = "Enter valid email.")]
+        public string Email { get; set; } = "info@example.com";
+        #endregion
+
+        public ActionResult Index()
+        {
+            var model = this.GetModel();
+            return View(this._viewName, model);
+        }
+
+        protected override void HandleUnknownAction(string actionName)
+        {
+            this.ActionInvoker.InvokeAction(this.ControllerContext, nameof(this.Index));
+        }
+
+        private ContactInfoModel GetModel()
+        {
+            return new ContactInfoModel()
+            {
+                Title = this.Title,
+                Email = this.Email,
+                PhoneNumber = this.PhoneNumber,
+                Address = this.Address
+            };
+        }
+    }
+}
---
+++ Apex.Web/Mvc/Models/ContactInfoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ApexWeb.Mvc.Models
+{
+    public class ContactInfoModel
+    {
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Title { get; internal set; }
+    }
+}
